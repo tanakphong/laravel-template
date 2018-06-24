c8e726f8b3df2e00e65b5ab16002cd30997f364a
tanakphong <t.phongsing@gmail.com>
2018-06-24 22:26:56 +0700

    install jwt

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,5 @@
 <?php
-
+/* 
 use Illuminate\Http\Request;
 
 /*
@@ -12,7 +12,27 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+}); */
+/*
+Route::post('signup', 'AuthController@register');
+Route::post('login', 'AuthController@login');
+Route::group(['prefix' => 'auth', 'middleware' => 'jwt.auth'], function () {
+    Route::get('user', 'AuthController@user');
+    Route::post('logout', 'AuthController@logout');
+});
+Route::middleware('jwt.refresh')->get('/token/refresh', 'AuthController@refresh');/* */
+Route::group([
+
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
 });
